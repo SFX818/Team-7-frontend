705333fdee4dd1321dc496ca5a88da312df86cc0
regularvanessaperson <vamavila4@gmail.com>
2020-12-30 23:28:08 -0600

    updated Favorites component can see in console log not rendering yet

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,21 +1,48 @@
+import React from 'react'
+
+//Helper
+import { viewFavoritePosts } from '../services/post.service'
+import { getCurrentUser } from '../services/auth.service'
+
 const Favorites = () => {
-    // const favePosts = () => {
-    //     // function that returns list of favePosts
-    //     return (
-    //         <ul>
-    //             <li>{posts.creator.name}</li>
-    //             <li>{posts.body}</li>
-    //         </ul>
+    // const [favorites, setFavorites]= useState([])
+    const currentUser = getCurrentUser()
+    const id = currentUser.id
 
-    //     )
-    // }
+    const favePosts = () => {
+        viewFavoritePosts(id).then(user => {
+            console.log(user.data)
+            return user.data.favoritePosts.map(post => (
+                <ul>
+                    <li key={post._id}>
+                        {post.body}
+                    </li>
+                </ul>
+             )) 
 
-    return <div>
-        Favorites
-        {/* this component will render a list of favorites by current user */}
-        
-         </div>
-    
+            })
+    }
+
+
+// const displayFavePost = () => {
+//     favePosts()
+//     console.log(favePosts())
+//   return(
+//     <ul>
+//     <li>{favorites}</li>
+//     </ul>
+//   )
+// }
+
+//     // function that returns list of favePosts
+
+
+return <div>
+    <h1>Favorites</h1>
+        {favePosts()}
+
+</div>
+
 }
 
 export default Favorites
