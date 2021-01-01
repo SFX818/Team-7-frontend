51ca029d34da16a95ad273bc9211c2f49b27e39e
regularvanessaperson <vamavila4@gmail.com>
2021-01-01 16:50:24 -0600

    able to render posts on Favorites.js

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,38 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 //Helper
 import { viewFavoritePosts } from '../services/post.service'
 import { getCurrentUser } from '../services/auth.service'
 
-const Favorites = () => {
-    // const [favorites, setFavorites]= useState([])
+const Favorites = (props) => {
+    console.log("what are props", props)
+    const [favorites, setFavorites]= useState([])
     const currentUser = getCurrentUser()
     const id = currentUser.id
     console.log(currentUser.id)
-    
-    const favePosts = () => {
-        viewFavoritePosts(id).then(user => {
-            console.log(user.data)
+    let postArray = []
+
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+       setLoading(false)
+       favePosts()
+    }, [])
+
+
+    async function favePosts() {
+      return await viewFavoritePosts(id).then(user => {
+            setLoading(false)
+            console.log(user.data.favoritePosts)
             return user.data.favoritePosts.map(post => (
                 <ul>
                     <li key={post._id}>
                         {post.body}
                     </li>
                 </ul>
-             )) 
-
-            })
+            ))
+        }).then(results=>{
+            setFavorites(results)
+        })
     }
 
 
+    // if (loading) {
+    //     return <div>
+    //         Loading
+    //     </div>
+    // }
 
 
+    return <div>
+        <h1>Favorites</h1>
+        {favorites}
 
-return <div>
-    <h1>Favorites</h1>
-        {favePosts()}
-
-</div>
+    </div>
 
 }
 
