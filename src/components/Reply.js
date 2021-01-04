2fc0bdccdce4be658f85c826c45bdb05f2e9b905
regularvanessaperson <vamavila4@gmail.com>
2021-01-04 14:30:15 -0600

    made notes on Reply component

--- src/components/Reply.js
+++ src/components/Reply.js
@@ -5,7 +5,9 @@ import { replyToPost } from '../services/auth.service.js'
 
 
 const EditPost = (props) => {
-
+//need to find the one post id that was chosen
+//make that one post render at the top of the page and replies under it
+//add post by current user as reply in the  array of the current posts 
     // const currentUser = getCurrentUser()
    
     return <h1>Reply Component</h1>
