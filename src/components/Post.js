11b6cf498a559c04302021528afb968f06bffecc
regularvanessaperson <vamavila4@gmail.com>
2021-01-04 14:31:57 -0600

    accepted both changes in merge conflict, two different buttons were overlapping

--- src/components/Post.js
+++ src/components/Post.js
@@ -155,12 +155,54 @@ const Post = (props) => {
                         <Button label="Unfavorite" handleClick={unfavorite}/>
                     )}
 
-                    {(!retweeted && !original && !repost) && (
+                    {retweeted && (
+                        <Button label="Un-retweet" handleClick={unretweet}/>
+                    )}
+
+
+                    {!retweeted && (
                         <Button label="Retweet" handleClick={retweet} />
                     )}
+                    <Link to={"/Reply"} className="nav-link">Replies</Link>
                 </div>)}
         </div>
         )   
+
+
+    // //helper functions
+    // return (
+    // <div>
+    // {/* determine how to render post based on booleans */}
+    // {post.isRepost && (
+    //     <span>Repost from {post.parentPost.creator}</span>
+    // )}
+
+    // {post.isReply && (
+    //     <span>Replied to {post.parentPost.creator}</span>
+    // )}
+
+    // {/*Link to view author profile */}
+    // {/* <Link to={`/profile/${props.creator.username}`}>{props.creator.username}</Link> */}
+
+    // {/* Post body */}
+    // <p>{props.body}</p>
+
+    // {/* button for favorite */}
+    //     <Button label='Favorite' handleClick={incrementFavorite}/>
+
+    // {/* reply form */}
+    // {/* <form onSubmit={replyToPost}>
+    //     <label>
+    //       Reply
+    //       <input type="text" value={this.state.value} onChange={this.handleReplyChange} />
+    //     </label>
+    //     <input type="submit" value="Reply" />
+    // </form> */}
+
+    // {/* button for repost */}
+    //     <Button label='Repost' handleClick={retweetPost}/>
+
+    // </div>
 }
 
 export default Post
---
+++ src/components/Reply.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+
+import { replyToPost } from '../services/auth.service.js'
+
+
+
+const EditPost = (props) => {
+
+    // const currentUser = getCurrentUser()
+   
+    return <h1>Reply Component</h1>
+}
+
+export default EditPost
